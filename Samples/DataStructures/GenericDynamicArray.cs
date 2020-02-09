e0f640eec8680d819dd51d934c205dd0d6b5c1c7
MartinWang <martinwang7963@gmail.com>
2020-02-09 20:38:30 +0800

    Add DynamicArray

--- Samples/DataStructures/GenericDynamicArray.cs
+++ Samples/DataStructures/GenericDynamicArray.cs
@@ -13,9 +13,14 @@ namespace Samples.DataStructures
             Datas = new T[InitialCapacity];
         }
 
-        public void Add()
+        public void Add(T data)
         {
-
+            if (Size == InitialCapacity)
+            {
+                Resize();
+            }
+            Datas[Size] = data;
+            Size++;
         }
 
         public T Get(int index)
@@ -34,6 +39,78 @@ namespace Samples.DataStructures
                 throw new IndexOutOfRangeException();
             }
             Datas[index] = val;
+        }
+
+        public void Insert(int index, T data)
+        {
+            if (Size == InitialCapacity)
+            {
+                Resize();
+            }
+
+            for (int i = Size; i > index; i--)
+            {
+                Datas[i] = Datas[i - 1];
+            }
+            Datas[index] = data;
+            Size++;
+        }
+
+        public void Delete(int index)
+        {
+            if (index >= InitialCapacity || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            for (int i = index; i < Size; i++)
+            {
+                Datas[i] = Datas[i + 1];
+            }
+            Size--;
+
+            if (index == Size)
+            {
+                Datas[index] = default(T);
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return Size == 0;
+        }
+
+        public bool Contains(T data)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                if (Datas[i].Equals(data))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int GetSize()
+        {
+            return Size;
+        }
+
+
+
+        private void Resize()
+        {
+            T[] newArray = new T[InitialCapacity * 2];
+
+            for (int i = 0; i < Size; i++)
+            {
+                newArray[i] = Datas[i];
+            }
+
+            Datas = newArray;
+            InitialCapacity = InitialCapacity * 2;
         }
 
     }
--- Test/GenericDynamicArrayTest.cs
+++ Test/GenericDynamicArrayTest.cs
@@ -22,5 +22,56 @@ namespace Test
             Assert.AreEqual(array.Get(0), "a");
 
         }
+
+        [Test]
+        public void Insert()
+        {
+            array.Add("a");
+            array.Add("b");
+            array.Add("c");
+
+            array.Insert(1, "d");
+            Assert.AreEqual(array.Get(0), "a");
+            Assert.AreEqual(array.Get(1), "d");
+            Assert.AreEqual(array.Get(2), "b");
+            Assert.AreEqual(array.Get(3), "c");
+        }
+
+        [Test]
+        public void Delete()
+        {
+            array.Add("a");
+            array.Add("b");
+            array.Add("c");
+            array.Add("d");
+            array.Add("e");
+            array.Add("f");
+
+            array.Delete(0);
+            array.Delete(5);
+            Assert.AreEqual(array.Get(0), "b");
+            Assert.AreEqual(array.Get(1), "c");
+            Assert.AreEqual(array.GetSize(), 4);
+        }
+
+        [Test]
+        public void IsEmpty()
+        {
+            Assert.AreEqual(array.IsEmpty(), true);
+        }
+
+        [Test]
+        public void Contains()
+        {
+            array.Add("a");
+            array.Add("b");
+            array.Add("c");
+            array.Add("d");
+            array.Add("e");
+            array.Add("f");
+
+            Assert.AreEqual(array.Contains("f"), true);
+            Assert.AreEqual(array.Contains("t"), false);
+        }
     }
 }
